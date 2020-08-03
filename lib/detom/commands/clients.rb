25326e1a6714a4407b1a85a60853d27325cfa1a7
njp <n.pearman@gmail.com>
2020-08-03 15:35:21 +0200

    clients command performs app directory setup

--- lib/detom/commands/clients.rb
+++ lib/detom/commands/clients.rb
@@ -1,10 +1,23 @@
 module Commands
   class Clients
+    DEFAULT_APP_DIRECTORY = "~/.detom"
+    
     def initialize
     end
 
     def call
+      create_root_directory unless root_directory_exists?
+
       puts "clients command ran"
     end
+
+    private
+      def root_directory_exists?
+        Dir.exist? DEFAULT_APP_DIRECTORY
+      end
+
+      def create_root_directory
+        Dir.mkdir DEFAULT_APP_DIRECTORY
+      end
   end
 end
--- spec/detom/commands/clients_spec.rb
+++ spec/detom/commands/clients_spec.rb
@@ -1,7 +1,23 @@
 require "detom/commands/clients"
 
 RSpec.describe Commands::Clients do
-  it "does something" do
-    expect(described_class.new).to_not be_nil
+  describe "#call" do
+    it "creates the app directory in ~/.detom if it does not already exist" do
+      dir = class_double("Dir").as_stubbed_const
+
+      expect(dir).to receive(:exist?).with("~/.detom").and_return false
+      expect(dir).to receive(:mkdir).with "~/.detom"
+
+      expect(described_class.new.call).to be_nil
+    end
+
+    it "does not create app directory if ~/.detom/ already exists" do
+      dir = class_double("Dir").as_stubbed_const
+
+      expect(dir).to receive(:exist?).with("~/.detom").and_return true
+      expect(dir).to_not receive(:mkdir).with "~/.detom"
+
+      expect(described_class.new.call).to be_nil
+    end
   end
 end
