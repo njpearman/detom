9e9dadaf130ea870ba9d20ea5f5659483fd1ee3d
njp <n.pearman@gmail.com>
2020-08-03 21:28:14 +0200

    Adds test for adding time to two clients

--- spec/detom/commands/record_spec.rb
+++ spec/detom/commands/record_spec.rb
@@ -2,7 +2,7 @@ require "detom/commands/record"
 
 describe Commands::Record do
   describe "#call" do
-    context "when recording time spent on a client" do
+    context "when recording time spent today on a client" do
       let(:store) { Hash.new }
 
       it "stores the time spent on the client" do
@@ -12,7 +12,7 @@ describe Commands::Record do
       end
     end
 
-    context "when recording time spent twice on a client" do
+    context "when recording time spent today twice on a client" do
       let(:store) { Hash.new }
 
       it "stores the time spent on the client" do
@@ -24,7 +24,7 @@ describe Commands::Record do
       end
     end
 
-    context "when recording time spent thrice on a client" do
+    context "when recording time spent today thrice on a client" do
       let(:store) { Hash.new }
 
       it "stores the time spent on the client" do
@@ -34,6 +34,19 @@ describe Commands::Record do
         record_command.call("foo_client", "39m")
         expect { record_command.call("foo_client", "92m") }.to output("Logged 92m for foo_client\n").to_stdout
         expect(store["foo_client"]).to eq({ today => [6, 39, 92] })
+      end
+    end
+
+    context "when recording time spent today on two clients" do
+      let(:store) { Hash.new }
+
+      it "stores the time spent on the clients" do
+        today = Time.now.strftime("%Y-%m-%d")
+        record_command = described_class.new(store)
+        record_command.call("foo_client", "6m")
+        record_command.call("raa_client", "39m")
+        expect(store["foo_client"]).to eq({ today => [6] })
+        expect(store["raa_client"]).to eq({ today => [39] })
       end
     end
   end
