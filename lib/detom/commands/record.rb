21dd1d670b1e7387311161bd54be4397c2e88508
njp <n.pearman@gmail.com>
2020-08-03 21:16:51 +0200

    Implements logging twice in one day for the same client

--- lib/detom/commands/record.rb
+++ lib/detom/commands/record.rb
@@ -5,7 +5,16 @@ module Commands
     end
 
     def call(client_name, time_to_log)
-      @store[client_name] = { Time.now.strftime("%Y-%m-%d") => time_to_log }
+      today = Time.now.strftime("%Y-%m-%d")
+      @store[client_name] = {} if @store[client_name].nil?
+
+      client = @store[client_name]
+
+      if client[today]
+        client[today] << time_to_log
+      else
+        client[today] = [ time_to_log ]
+      end
     end
   end
 end
--- spec/detom/commands/record_spec.rb
+++ spec/detom/commands/record_spec.rb
@@ -2,12 +2,26 @@ require "detom/commands/record"
 
 describe Commands::Record do
   describe "#call" do
-    let(:store) { Hash.new }
+    context "when recording time spent on a client" do
+      let(:store) { Hash.new }
 
-    it "does something" do
-      today = Time.now.strftime("%Y-%m-%d")
-      described_class.new(store).call("foo_client", "6m")
-      expect(store["foo_client"]).to eq({ today => "6m" })
+      it "stores the time spent on the client" do
+        today = Time.now.strftime("%Y-%m-%d")
+        described_class.new(store).call("foo_client", "6m")
+        expect(store["foo_client"]).to eq({ today => ["6m"] })
+      end
+    end
+
+    context "when recording time spent twice on a client" do
+      let(:store) { Hash.new }
+
+      it "stores the time spent on the client" do
+        today = Time.now.strftime("%Y-%m-%d")
+        record_command = described_class.new(store)
+        record_command.call("foo_client", "6m")
+        record_command.call("foo_client", "39m")
+        expect(store["foo_client"]).to eq({ today => ["6m", "39m"] })
+      end
     end
   end
 end
