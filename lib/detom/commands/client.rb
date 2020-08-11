da332d9515957434df7663afc57a748132f5e0d2
njp <n.pearman@gmail.com>
2020-08-11 17:39:01 +0200

    Implements client for one time today

--- lib/detom/commands/client.rb
+++ lib/detom/commands/client.rb
@@ -1,7 +1,20 @@
 module Commands
   class Client
+    def initialize(store)
+      @store = store
+    end
+
     def call(client_name)
-      $stdout.puts "No time logged against #{client_name}"
+      client = @store[client_name]
+
+      if client
+        output = client.sort.map do |day, times|
+          "#{day}: #{times.join}m"
+        end.join "\n"
+        $stdout.puts output
+      else
+        $stdout.puts "No time logged against #{client_name}" 
+      end
     end
   end
 end
--- spec/detom/commands/client_spec.rb
+++ spec/detom/commands/client_spec.rb
@@ -2,9 +2,27 @@ require "detom/commands/client"
 
 describe Commands::Client do
   describe "#call" do
-    it do
-      expect { described_class.new.call "foo_client" }.to output("No time logged against foo_client\n").to_stdout
+    subject { described_class.new(store).call(client_name) }
 
+    let(:store) { {} }
+
+    context "with a client that has no time logged" do
+      let(:client_name) { "foo_client" }
+      it do
+        expect { subject }.to output("No time logged against foo_client\n").to_stdout
+      end
+    end
+
+    context "with a client that has one entry today" do
+      let(:client_name) { "foo_client" }
+
+      let(:store) { { "foo_client" => { Time.now.strftime("%Y-%m-%d") => [50] } } }
+      it do
+        expected_output = <<OUT
+#{Time.now.strftime("%Y-%m-%d")}: 50m
+OUT
+        expect { subject }.to output(expected_output).to_stdout
+      end
     end
   end
 end
