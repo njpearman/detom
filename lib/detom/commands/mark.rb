0642e3adad707a304d3f12b5795eca59e837be12
njp <n.pearman@gmail.com>
2020-10-06 12:33:23 +0200

    Adds list command and clear option for mark

--- lib/detom/commands/mark.rb
+++ lib/detom/commands/mark.rb
@@ -1,7 +1,64 @@
 module Commands
   class Mark
-    def call
-      puts Time.now.strftime("%H:%M")
+    def initialize
+      @filepath = File.join YamlFileStore::DEFAULT_APP_DIRECTORY, ".marks"
+    end
+
+    def call(client)
+      mark = Time.now
+      # create mark file unless exists
+      load_marks
+
+      # set value for client
+      @marks[client] = mark
+
+      # replace contents of mark file
+      File.open(@filepath, "w") {|file| YAML.dump @marks, file }
+
+      STDOUT.puts "Marked #{client} at #{mark.strftime("%H:%M")}"
+    end
+    
+    def clear(client)
+      load_marks
+
+      put_no_marks(client) && return unless marked?(client)
+      
+      @marks.delete client
+      File.open(@filepath, "w") {|file| YAML.dump @marks, file }
+
+      STDOUT.puts "Removed mark for #{client}"
+    end
+
+    def list
+      load_marks
+
+      if @marks.any?
+        @marks.each do |mark|
+          time = mark[1].strftime("%H:%M (%Y-%m-%d)")
+          STDOUT.puts "#{mark[0]}: #{time}"
+        end
+      else
+        STDOUT.puts "Nothing is currently marked"
+      end
+    end
+
+    private
+    def marked?(client)
+      @marks.keys.include? client
+    end
+
+    def load_marks
+      File.exist?(@filepath).tap do |exist|
+        if exist
+          @marks = YAML.load File.read(@filepath) 
+        else
+          @marks = {}
+        end
+      end
+    end
+    
+    def puts_no_mark(client)
+      STDOUT.puts "No mark set for #{client}"
     end
   end
 end
